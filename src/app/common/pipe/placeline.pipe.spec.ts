4fe0a3df3aed88630244f50b43183ba305ac45e2
Drew Keller <dkeller@captechventures.com>
2018-03-02 15:14:16 -0500

    Test PlacelinePipe

--- src/app/common/pipe/placeline.pipe.spec.ts
+++ src/app/common/pipe/placeline.pipe.spec.ts
@@ -1,8 +1,29 @@
 import { PlacelinePipe } from "./placeline.pipe";
 
 describe("PlacelinePipe", () => {
+  const pipe = new PlacelinePipe();
+
+  const articleStub = {
+    categoryId: 0,
+    fullStory: "Full Story",
+    hasVideoPlaceholder: false,
+    headLine: "Headline",
+    id: 0,
+    location: "Location",
+    nsfw: false,
+    numberOfImages: 0,
+    relatedArticleIds: [],
+    snippet: "Snippet"
+  };
+
   it("creates an instance", () => {
-    const pipe = new PlacelinePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  describe("when passed an article", () => {
+    it("returns the placeline in upper case", () => {
+      const placeline = pipe.transform(articleStub);
+      expect(placeline).toBe(articleStub.location.toUpperCase());
+    });
   });
 });
